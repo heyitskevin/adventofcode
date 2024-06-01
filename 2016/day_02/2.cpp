84f399249450ddd262b4ba4d1ede56bb6a9efad1
kevin <kevinliu314@gmail.com>
2024-06-01 17:59:32 -0500

    day2p2 peko

--- 2016/day_02/2.cpp
+++ 2016/day_02/2.cpp
@@ -34,16 +34,28 @@ std::vector<std::vector<char> > read_file() {
 }
 
 pair<int, int> do_move(pair<int, int> start, char move) {
+    char board[5][5] = {
+        {'0', '0', '1', '0', '0'},
+        {'0', '2', '3', '4', '0'},
+        {'5', '6', '7', '8', '9'},
+        {'0', 'A', 'B', 'C', '0'},
+        {'0', '0', 'D', '0', '0'}
+    };
+
     switch(move) {
         case 'U':
             if (start.first == 0) {
+                break;
+            } else if (board[start.first-1][start.second] == '0') {
                 break;
             } else {
                 start.first = start.first - 1;
                 break;
             }
         case 'D':
-            if (start.first == 2) {
+            if (start.first == 4) {
+                break;
+            } else if (board[start.first+1][start.second] == '0') {
                 break;
             } else {
                 start.first = start.first + 1;
@@ -52,18 +64,21 @@ pair<int, int> do_move(pair<int, int> start, char move) {
         case 'L':
             if (start.second == 0) {
                 break;
+            } else if(board[start.first][start.second-1] == '0') {
+                break;
             } else {
                 start.second = start.second - 1;
                 break;
             }
         case 'R':
-            if (start.second == 2) {
+            if (start.second == 4) {
+                break;
+            } else if (board[start.first][start.second+1] == '0') { 
                 break;
             } else {
                 start.second = start.second + 1;
             }
     }
-    
     return start;
 }
 
@@ -79,16 +94,20 @@ pair<int, int> process_line(vector<char> line, pair<int, int> start_coords) {
 
 int main() {
     vector<vector<char> > instructions = read_file();
-
-    int board[3][3] = {
-        {1, 2, 3},
-        {4, 5, 6},
-        {7, 8, 9}
+    
+    char board[5][5] = { // Double Instantiation, don't know enough about global vars to give a damn about fixing this
+        {'0', '0', '1', '0', '0'},
+        {'0', '2', '3', '4', '0'},
+        {'5', '6', '7', '8', '9'},
+        {'0', 'A', 'B', 'C', '0'},
+        {'0', '0', 'D', '0', '0'}
     };
-    pair<int, int> coords = make_pair(1, 1);
+
+    pair<int, int> coords = make_pair(2, 0);
+    
     for (vector<char> line : instructions) {
         coords = process_line(line, coords);
-        cout << board[coords.first][coords.second] << endl;
+        cout << board[coords.first][coords.second] << ' ';
     }
 
     return 0;
