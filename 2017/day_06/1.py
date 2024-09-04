803d44ad9b46cc53b27f5be5d8dd819dd0942324
kevin <kevinliu314@gmail.com>
2024-09-03 21:28:57 -0500

    day 6 p1 p2 peko

---
+++ 2017/day_06/1.py
@@ -0,0 +1,32 @@
+FILENAME = 'input.txt'
+
+def read_file():
+    with open(FILENAME) as f:
+        return [int(x) for x in f.read().split()]
+
+def func():
+    banks = read_file()
+    
+    visited = set()
+    steps = 0
+    while True:
+        if tuple(banks) in visited:
+            break
+        visited.add(tuple(banks))
+        max_val = max(banks)
+        ix = 0
+        for x in range(len(banks)):
+            if banks[x] == max_val:
+                ix = x
+                break
+        banks[ix] = 0
+        ix += 1
+        while max_val:
+            banks[ix % len(banks)] += 1
+            max_val -= 1
+            ix += 1
+        
+        steps += 1
+    return steps
+
+print(func())
