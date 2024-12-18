9720331c1eb1482219fc51124bf50c6905d7152e
kevin <kevinliu314@gmail.com>
2024-12-18 10:22:53 -0800

    day 10 peko

---
+++ 2024/day_10/2.py
@@ -0,0 +1,50 @@
+def readfile():
+    res = []
+    with open("input.txt") as f:
+        for ln in f.read().split('\n'):
+            res.append(list(ln.strip()))
+    
+    return res
+
+def trailheads(grid):
+    th = []
+    for ix, row in enumerate(grid):
+        for yx, col in enumerate(row):
+            if col == '0':
+                th.append((ix, yx))
+    return th
+
+def traverse(grid, start_row, start_col):
+    dirs = [(-1, 0), (1, 0), (0, -1),  (0, 1)] # up, down, left, right
+    score = 0
+    q = [(start_row, start_col)]
+    height = len(grid)
+    width = len(grid[0])
+    while q:
+        row, col  = q.pop(0)
+        val = grid[row][col]
+        if val == '9':
+            score += 1
+            continue
+        else:
+            next_val = str(int(val) + 1)
+            for d in dirs:
+                dy, dx = d
+                d_r = row + dy
+                d_c = col + dx
+                if 0 <= d_r < height and 0 <= d_c < width and grid[d_r][d_c] == next_val:
+                    q.append((d_r, d_c))
+    
+    return score
+
+
+def main():
+    grid = readfile()
+    tr = trailheads(grid)
+    res = 0
+    for t in tr:
+        r, c = t
+        res += traverse(grid, r, c)
+    return res
+
+print(main())
